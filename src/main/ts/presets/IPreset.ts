4827ab822b336a2e76dd6661f595dd540a8fcb23
brian teeman <brian@teeman.net>
2022-11-11 10:22:45 +0000

    Remove align self options

--- src/main/ts/presets/IPreset.ts
+++ src/main/ts/presets/IPreset.ts
@@ -14,7 +14,6 @@ interface Breakpoint {
 
 interface IPreset {
     columns: Column[];
-    alignSelf: Column[];
     breakpoints: Breakpoint[];
     /**
      * Gets style url
@@ -30,13 +29,6 @@ interface IPreset {
      */
     columnClassRegex(columnPreffix: string): RegExp;
     /**
-     * Returns regxp for align-self class
-     *
-     * @param {string} columnPreffix
-     * @return {RegExp}
-     */
-     alignSelfClassRegex(columnPreffix: string): RegExp;
-    /**
      * Builds column class based on prefix and breakpoint
      *
      * @param {string} prefix
@@ -44,14 +36,6 @@ interface IPreset {
      * @return {string}
      */
     columnClass(prefix: string, breakpoint: string): string;
-    /**
-     * Builds self-align class based on prefix and breakpoint
-     *
-     * @param {string} prefix
-     * @param {string} breakpoint
-     * @return {string}
-     */
-     selfAlignClass(prefix: string, alignment: string): string;
     /**
      * Check if class is column
      *
